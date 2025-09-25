974b0f1f2bd7e9ab0e00ea17a689bfb5d188234d
Bradley Fuller <37586752+BradF-99@users.noreply.github.com>
2025-09-25 22:52:31 +1000

    fix(sql): add database use to insert realm script

--- sql/03-insert-realm.sql
+++ sql/03-insert-realm.sql
@@ -1,3 +1,4 @@
+USE classicrealmd;
 DELETE FROM realmlist WHERE id=1;
 INSERT INTO realmlist (id, name, address, port, icon, realmflags, timezone, allowedSecurityLevel)
 VALUES ('1', 'REPLACE WITH REALM NAME', 'REPLACE WITH **PUBLIC FACING IP ADDRESS**!!', '8085', '1', '0', '3', '0');
